e96f26a5a8d5039a5276fb0eccabda980ae54d02
KORE ANIL <100741955+kore-Anil-716@users.noreply.github.com>
2025-02-19 22:48:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.static('./public'))
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://vercel.com/kore-anils-projects/video-nest-frontend',
     methods: ['POST', 'GET'],
     credentials: true
 }));
